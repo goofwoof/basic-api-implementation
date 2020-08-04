9602ff9b53e8de3311811d05e6ab66a031b213ec
goofwoof <heli0086@outlook.com>
2020-08-04 20:49:43 +0800

    复现课堂Demo 并实现 需求4修改、5删除

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,20 +1,60 @@
 package com.thoughtworks.rslist.api;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.thoughtworks.rslist.domain.RsEvent;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class RsController {
-  private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+  private List<RsEvent> rsList = initalRsEvent();
 
-  @RequestMapping(value = "/re/list", method = RequestMethod.GET)
-  @ResponseBody
-  public String getStringOfreList(){
-    return this.rsList.toString();
+  private List<RsEvent> initalRsEvent() {
+    List<RsEvent> rsList = new ArrayList<>();
+    rsList.add(new RsEvent("第一条事件", "无标签"));
+    rsList.add(new RsEvent("第二条事件", "无标签"));
+    rsList.add(new RsEvent("第三条事件", "无标签"));
+    return rsList;
   }
+
+  @GetMapping("/rs/list")
+  public List<RsEvent> getStringOfreList(@RequestParam(required = false) Integer start, @RequestParam(required = false) Integer end){
+    if(start != null || end != null){
+      return rsList.subList(start - 1, end);
+    }
+    return this.rsList;
+  }
+
+
+  @GetMapping("/rs/{index}")
+  public RsEvent getStringOfSelectedList(@PathVariable int index){
+    return this.rsList.get(index - 1);
+  }
+
+  @PostMapping("/rs/add")
+  public void addEvent(@RequestBody RsEvent rsEvent){
+    rsList.add(rsEvent);
+  }
+
+  @PostMapping("rs/change/{index}")
+  public RsEvent changeEvent(@PathVariable int index, @RequestParam(required = false) String eventName, @RequestParam(required = false) String keyWord){
+    if(eventName != null){
+      rsList.get(index-1).setEventName(eventName);
+    }
+    if(keyWord != null){
+      rsList.get(index-1).setKeyWord(keyWord);
+    }
+    return rsList.get(index-1);
+  }
+
+  @GetMapping("rs/delete/{index}")
+  public RsEvent deleteEvent(@PathVariable int index){
+      return rsList.remove(index - 1);
+  }
+
+
+
+
 }
--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -1,27 +1,95 @@
 package com.thoughtworks.rslist;
 
-import org.apache.tomcat.util.descriptor.web.FragmentJarScannerCallback;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thoughtworks.rslist.domain.RsEvent;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class RsListApplicationTests {
     @Autowired
     MockMvc mockMvc;
 
     @Test
-    void contextLoads() throws Exception {
-        mockMvc.perform(get("/re/list"))
+    @Order(1)
+    void T1_contextLoads() throws Exception {
+        mockMvc.perform(get("/rs/list"))
                 .andExpect(status().isOk())
-                .andExpect(content().string("[第一条事件, 第二条事件, 第三条事件]"));
+                .andExpect(jsonPath("$.length()").value(3))
+                .andExpect(jsonPath("$[0].eventName").value("第一条事件"))
+                .andExpect(jsonPath("$[0].keyWord").value("无标签"))
+                .andExpect(jsonPath("$[1].eventName").value("第二条事件"))
+                .andExpect(jsonPath("$[1].keyWord").value("无标签"))
+                .andExpect(jsonPath("$[2].eventName").value("第三条事件"))
+                .andExpect(jsonPath("$[2].keyWord").value("无标签"));
     }
+
+    @Test
+    @Order(2)
+    void T2_get_select_one_event() throws Exception {
+        mockMvc.perform(get("/rs/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.eventName").value("第一条事件"))
+                .andExpect(jsonPath("$.keyWord").value("无标签"));
+    }
+    
+    @Test
+    @Order(3)
+    void T3_get_list_selected_enents() throws Exception {
+        mockMvc.perform(get("/rs/list?start=1&end=2"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].eventName").value("第一条事件"))
+                .andExpect(jsonPath("$[0].keyWord").value("无标签"))
+                .andExpect(jsonPath("$[1].eventName").value("第二条事件"))
+                .andExpect(jsonPath("$[1].keyWord").value("无标签"));
+    }
+    
+    
+    @Test
+    @Order(4)
+    void T4_add_event() throws Exception {
+        String jsonParam = new ObjectMapper().writeValueAsString(new RsEvent("猪肉涨价了", "经济"));
+        mockMvc.perform(post("/rs/add").content(jsonParam).contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/rs/list"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(4));
+    }
+
+    @Test
+    @Order(5)
+    void T5_chang_event() throws Exception {
+        mockMvc.perform(post("/rs/change/1")
+                .param("eventName", "特朗普辞职了")
+                .param("keyWord", "时政")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.eventName").value("特朗普辞职了"))
+                .andExpect(jsonPath("$.keyWord").value("时政"));
+    }
+
+    @Test
+    @Order(6)
+    void T6_delete_event() throws Exception {
+        mockMvc.perform(get("/rs/delete/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.eventName").value("第一条事件"))
+                .andExpect(jsonPath("$.keyWord").value("无标签"));
+    }
+
 
 }
