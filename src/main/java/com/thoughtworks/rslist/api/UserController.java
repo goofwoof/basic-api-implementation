e7cb59052cdfbd7ff20c1254fa28989d6e9b090c
goofwoof <heli0086@outlook.com>
2020-08-07 14:39:43 +0800

    User 重构， controller 业务逻辑向service迁移

--- src/main/java/com/thoughtworks/rslist/api/UserController.java
+++ src/main/java/com/thoughtworks/rslist/api/UserController.java
@@ -3,6 +3,7 @@ package com.thoughtworks.rslist.api;
 import com.thoughtworks.rslist.domain.User;
 import com.thoughtworks.rslist.dto.UserDto;
 import com.thoughtworks.rslist.repository.UserRepository;
+import com.thoughtworks.rslist.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,36 +13,27 @@ import javax.validation.Valid;
 @RestController
 public class UserController {
     @Autowired
-    UserRepository userRepository;
+    UserService userService;
 
     @PostMapping("/user")
     public ResponseEntity addUser(@RequestBody @Valid User user){
-        UserDto userDto = UserDto.builder().userName(user.getUserName())
-                .age(user.getAge()).email(user.getEmail()).gender(user.getGender())
-                .phone(user.getPhone()).build();
-        userRepository.save(userDto);
+        userService.addNewUser(user);
         return ResponseEntity.created(null).build();
     }
 
     @GetMapping("/users")
     public ResponseEntity getUsers(){
-
-        return ResponseEntity.ok(userRepository.findAll());
+        return ResponseEntity.ok(userService.getAllUsers());
     }
 
     @GetMapping("/get/user/{index}")
     public ResponseEntity getUser(@PathVariable int index){
-
-        return ResponseEntity.ok(userRepository.findById(index));
+        return ResponseEntity.ok(userService.getUserById(index));
     }
 
     @PostMapping("/user/delete/{index}")
     public ResponseEntity deleteUser(@PathVariable int index){
-        try{
-            userRepository.deleteById(index);
-        }catch (Exception e){
-            return ResponseEntity.badRequest().header("error", "error index").body(e);
-        }
-        return ResponseEntity.ok(userRepository.findAll());
+        userService.deleteUserById(index);
+        return ResponseEntity.ok(userService.getAllUsers());
     }
 }
--- src/main/java/com/thoughtworks/rslist/service/UserService.java
+++ src/main/java/com/thoughtworks/rslist/service/UserService.java
@@ -1,10 +1,15 @@
 package com.thoughtworks.rslist.service;
 
+import com.thoughtworks.rslist.domain.User;
 import com.thoughtworks.rslist.dto.UserDto;
 import com.thoughtworks.rslist.exception.ErrorEmptyObjectException;
 import com.thoughtworks.rslist.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -20,5 +25,49 @@ public class UserService {
             throw new ErrorEmptyObjectException("Unknown UserID");
         }
         return userRepository.findById(userId).get();
+    }
+
+    public void addNewUser(User user) {
+        UserDto userDto = UserDto.builder().userName(user.getUserName())
+                .age(user.getAge()).email(user.getEmail()).gender(user.getGender())
+                .phone(user.getPhone()).build();
+        addNewUser(userDto);
+    }
+
+    public void addNewUser(UserDto userDto) {
+        userRepository.save(userDto);
+    }
+
+    public List<UserDto> getAllUsersDto() {
+        return userRepository.findAll();
+    }
+
+    public List<User> getAllUsers() {
+        return getAllUsersDto().stream().map(userDto ->
+                User.builder().voteNum(userDto.getVoteNum())
+                        .userName(userDto.getUserName())
+                        .age(userDto.getAge())
+                        .email(userDto.getEmail())
+                        .gender(userDto.getGender())
+                        .phone(userDto.getPhone()).build())
+                .collect(Collectors.toList());
+    }
+
+    public User getUserById(int userId) {
+        UserDto userDto = getUserDtoById(userId);
+        return User.builder().voteNum(userDto.getVoteNum())
+                .userName(userDto.getUserName())
+                .age(userDto.getAge())
+                .email(userDto.getEmail())
+                .gender(userDto.getGender())
+                .phone(userDto.getPhone()).build();
+    }
+
+    public void deleteUserById(int userId) {
+        try{
+            userRepository.deleteById(userId);
+        }catch (Exception e){
+            throw new Error("error userId");
+        }
     }
 }
